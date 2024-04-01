1b7c86df64862b654ea37a2d1bbdaf0607a687d8
Yevhen Piven <evgeniy13.91a@gmail.com>
2024-04-01 13:01:28 +0200

    initial commit

---
+++ Task2.2JDBCApi/src/test/java/ua/foxminded/yevhenpiven/Task2/JDBCApi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.foxminded.yevhenpiven.Task2.JDBCApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
